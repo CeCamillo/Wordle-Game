23de8afc1e577638670ea6d2133821b872a1998a
CeCamillo <cesarcamillo2212@gmail.com>
2024-01-24 12:07:02 -0300

    feat: add controlled input and finished exercise 1

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React from "react";
 
 function GuessInput() {
-  return <div></div>;
+  const [guess, setGuess] = React.useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.info({ guess });
+    setGuess("");
+  }
+
+  return (
+    <div>
+      <form class="guess-input-wrapper" onSubmit={handleSubmit}>
+        <label for="guess-input">Enter guess:</label>
+        <input
+          id="guess-input"
+          type="text"
+          minLength={5}
+          maxLength={5}
+          value={guess}
+          onChange={(event) => {
+            setGuess(event.target.value.toUpperCase());
+          }}
+        />
+      </form>
+    </div>
+  );
 }
 
 export default GuessInput;
