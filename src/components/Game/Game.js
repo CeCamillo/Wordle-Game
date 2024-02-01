54db5ad359400423dfada5567205452d56651f3a
CeCamillo <cesarcamillo2212@gmail.com>
2024-02-01 12:27:53 -0300

    feat: finish game structure and exercise 3 and fix code structure for components

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,9 +1,12 @@
 import React from "react";
 import GuessInput from "../GuessInput";
 import GuessResults from "../GuessResults";
+import Guess from "../Guess/Guess";
+import { range } from '../../utils';
 
 import { sample } from "../../utils";
 import { WORDS } from "../../data";
+import { NUM_OF_GUESSES_ALLOWED } from "../../constants"
 
 
 // Pick a random word on every pageload.
--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import { range } from '../../utils';
+import { NUM_OF_GUESSES_ALLOWED } from '../../constants'
+import Guess from '../Guess/Guess';
 
 function guessResults({ guesses }) {
   return <div className='guess-results'>
-    {guesses.map((guess) => (
-      <p className='guess' key={crypto.randomUUID()}>{guess}</p>
+    {range(NUM_OF_GUESSES_ALLOWED).map((num) => (
+      <Guess key={num} value={guesses[num]} />
     ))}
-
   </div>;
 }
 
